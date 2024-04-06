fc57f24d884ab755933a87c1087494609201a9ba
Radovan Bast <bast@users.noreply.github.com>
2024-04-06 17:16:07 +0200

    fix few problems with shellcheck

--- history-window.sh
+++ history-window.sh
@@ -21,9 +21,10 @@ print_line () {
     echo -ne "\033[${row};${col}H"
 
     local string_length=${#4}
-    local window_with_buffer=$((${WINDOW_WIDTH} + 2))
-    local padding_length=$((${window_with_buffer} - ${string_length}))
-    local padding=$(printf '%.0s ' $(seq 1 ${padding_length}))
+    local window_with_buffer=$((WINDOW_WIDTH + 2))
+    local padding_length=$((window_with_buffer - string_length))
+    local padding
+    padding=$(printf '%.0s ' $(seq 1 ${padding_length}))
     local string_with_padding="${4}${padding}"
     local trimmed_string="${string_with_padding:0:${window_with_buffer}}"
 
@@ -41,21 +42,25 @@ show_history_block() {
     color=$(tput setaf ${HISTORY_FONT_COLOR})
     echo -n "${color}"
 
-    local terminal_width=$(tput cols)
-    local col=$((${terminal_width} - ${WINDOW_WIDTH} - 4))
-    local width_with_padding=$((${WINDOW_WIDTH} + 2))
+    local terminal_width
+    terminal_width=$(tput cols)
+    local col=$((terminal_width - WINDOW_WIDTH - 4))
+    local width_with_padding=$((WINDOW_WIDTH + 2))
 
-    local repeated_chars=$(printf '%.0s_' $(seq 1 ${width_with_padding}))
+    local repeated_chars
+    repeated_chars=$(printf '%.0s_' $(seq 1 ${width_with_padding}))
     print_line 0 ${col} " " "${repeated_chars}" " "
 
-    for (( i = 1; i <= ${NUM_HISTORY_LINES}; i++ )); do
-        local depth=$((${NUM_HISTORY_LINES} + 1 - $i))
-        local line=$(tail -n ${depth} ${BASH_LOG} | head -n 1)
+    for (( i = 1; i <= NUM_HISTORY_LINES; i++ )); do
+        local depth=$((NUM_HISTORY_LINES + 1 - i))
+        local line
+        line=$(tail -n ${depth} ${BASH_LOG} | head -n 1)
         print_line $i ${col} "|" " ${line}" "|"
     done
 
-    local repeated_chars=$(printf '%.0s‾' $(seq 1 ${width_with_padding}))
-    print_line $((1 + ${NUM_HISTORY_LINES})) ${col} " " "${repeated_chars}" " "
+    local repeated_chars
+    repeated_chars=$(printf '%.0s‾' $(seq 1 ${width_with_padding}))
+    print_line $((1 + NUM_HISTORY_LINES)) ${col} " " "${repeated_chars}" " "
 
     # restore the default font color
     echo -n "${default_color}"
@@ -68,7 +73,8 @@ bash_log_commands () {
     # https://superuser.com/questions/175799
     [ -n "$COMP_LINE" ] && return  # do nothing if completing
     [[ "$PROMPT_COMMAND" =~ "$BASH_COMMAND" ]] && return # don't cause a preexec for $PROMPT_COMMAND
-    local this_command=`HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
+    local this_command
+    this_command=$(HISTTIMEFORMAT='' history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//");
     echo "$this_command" >> "$BASH_LOG"
 }
 
